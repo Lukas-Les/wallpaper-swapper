2df8010715604219eedadae1cd5423046fe609cb
stabmeldys <lukas.les93@gmail.com>
2024-11-24 13:48:31 +0200

    cron env limits resolved

--- install.sh
+++ install.sh
@@ -2,8 +2,15 @@
 
 CRON_SHEDULE="0 * * * *"
 APP_LOCATION="$HOME/.local/bin/wallpaper-swap"
+
+CRON_JOB="$CRON_SHEDULE $APP_LOCATION"
+CURRENT_CRONTAB=$(crontab -l 2>/dev/null)
+
 cp ./wallpaper-swap.sh "$APP_LOCATION"
 chmod +x "$APP_LOCATION"
 echo "command moved to $APP_LOCATION"
-echo "seting up crotab"
-(crontab -l 2>/dev/null; echo "$CRON_SHEDULE $APP_LOCATION") | crontab -
+
+if ! echo "$CURRENT_CRONTAB" | grep -qF "$CRON_JOB"; then
+    echo "seting up crotab"
+    (crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
+fi
--- wallpaper-swap.sh
+++ wallpaper-swap.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 WALLPAPERS_DIR="$HOME/Pictures/Wallpapers/"
-DISPLAY=:0
-DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u)/bus"
+export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u)/bus"
 
 ensure_wallpapers_dir() {
     if [ ! -d "$WALLPAPERS_DIR" ]; then
