294c86fbecdaaf907c63859ff89883c82e94b889
stabmeldys <lukas.les93@gmail.com>
2024-11-24 16:31:28 +0200

    added swap on reboot

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ CRON_SHEDULE="0 * * * *"
 APP_LOCATION="$HOME/.local/bin/wallpaper-swap"
 
 CRON_JOB="$CRON_SHEDULE $APP_LOCATION"
+BOOT_CRON_JOB="@reboot $APP_LOCATION"
 CURRENT_CRONTAB=$(crontab -l 2>/dev/null)
 
 cp ./wallpaper-swap.sh "$APP_LOCATION"
@@ -13,4 +14,9 @@ echo "command moved to $APP_LOCATION"
 if ! echo "$CURRENT_CRONTAB" | grep -qF "$CRON_JOB"; then
     echo "seting up crotab"
     (crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
+fi
+
+if ! echo "$CURRENT_CRONTAB" | grep -qF "$BOOT_CRON_JOB"; then
+    echo "seting up boot crotab"
+    (crontab -l 2>/dev/null; echo "$BOOT_CRON_JOB") | crontab -
 fi
